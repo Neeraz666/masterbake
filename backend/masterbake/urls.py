43278d7d6e7a946fb272d0f481a161630de2236c
Niraj Pandey <neeraz.pandey2058@gmail.com>
2024-05-13 22:01:36 +0545

    User models created.

--- backend/masterbake/urls.py
+++ backend/masterbake/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_pair'),
-    path('api/users/', include('users.urls')),
+    # path('api/users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
