06853f733118a4f91d016ba466e4492a8a07277e
Niraj Pandey <neeraz.pandey2058@gmail.com>
2024-05-20 09:04:35 +0545

    Made some changes.

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,9 +1,62 @@
 import React, { useState } from 'react';
 import '../css/login.css';
+import axios from "axios";
 
 export const Login = () => {
     const [isLoginForm, setIsLoginForm] = useState(true);
     const [showPassword, setShowPassword] = useState(false);
+
+
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [profilephoto, setProfilephoto] = useState(null);
+    const [password, setPassword] = useState('');
+    const [password1, setPassword1] = useState('');
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setProfilephoto(file);
+    }
+
+    const submitsignup = async e => {
+        e.preventDefault();
+
+        if (password !== password1) {
+            alert('Passwords do not match!');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('email', email);
+        formData.append('username', username);
+        formData.append('firstname', firstname);
+        formData.append('lastname', lastname);
+        formData.append('profilephoto', profilephoto);
+        formData.append('password', password);
+        formData.append('password1', password1);
+
+        try{
+            const {data} = await axios.post('http://localhost:8000/api/users/signup', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            window.location.href = '/login'
+        }
+
+        catch (error) {
+            if(error.response && error.response.data){
+                alert (error.response.data.error || error.response.data);
+            }
+            else {
+                console.error('Error signing up: ', error);
+                alert('An error occured while signing up. Please try again.');
+            }
+        }
+    }
+
 
     const toggleForm = () => {
         setIsLoginForm(!isLoginForm);
@@ -21,14 +74,14 @@ export const Login = () => {
                         <form action="#">
                             <h2>Login</h2>
                             <div className="input_box">
-                                <input type="email" placeholder="Enter your email" required />
+                                <input type="email" placeholder="Enter your email" value={email}  required onChange={(e) => setEmail(e.target.value)} />
                                 <i className="uil uil-envelope-alt email"></i>
                             </div>
                             <div className="input_box">
                                 <input 
                                     type={showPassword ? "text" : "password"} 
                                     placeholder="Enter your password" 
-                                    required 
+                                    value={password} required onChange={(e) => setPassword(e.target.value)}  
                                 />
                                 <i className="uil uil-lock password"></i>
                                 <i 
@@ -52,14 +105,29 @@ export const Login = () => {
                         <form action="#">
                             <h2>Signup</h2>
                             <div className="input_box">
-                                <input type="email" placeholder="Enter your email" required />
+                                <input type="email" placeholder="Enter your email" value={email} required onChange={(e) => setEmail(e.target.value)} />
+                                <i className="uil uil-envelope-alt email"></i>
+                            </div>
+                            <div className="input_box">
+                                <input type="text" placeholder="Enter your username" value={username} required onChange={(e) => setUsername(e.target.value)} />
+                                <i className="uil uil-envelope-alt email"></i>
+                            </div>
+                            <div className="input_box">
+                                <input type="text" placeholder="Enter your First Name" value={firstname} required onChange={(e) => setFirstname(e.target.value)} />
+                                <i className="uil uil-envelope-alt email"></i>
+                            </div>
+                            <div className="input_box">
+                                <input type="text" placeholder="Enter your Last Name" value={lastname} required onChange={(e) => setLastname(e.target.value)} />
+                                <i className="uil uil-envelope-alt email"></i>
+                            </div>
+                            <div className="input_box">
+                                <input type="file" accept='image/*' required onChange={handleFileChange} />
                                 <i className="uil uil-envelope-alt email"></i>
                             </div>
                             <div className="input_box">
                                 <input 
                                     type={showPassword ? "text" : "password"} 
-                                    placeholder="Create password" 
-                                    required 
+                                    placeholder="Create password" value={password} required onChange={(e) => setPassword(e.target.value)} 
                                 />
                                 <i className="uil uil-lock password"></i>
                                 <i 
@@ -70,8 +138,8 @@ export const Login = () => {
                             <div className="input_box">
                                 <input 
                                     type={showPassword ? "text" : "password"} 
-                                    placeholder="Confirm password" 
-                                    required 
+                                    placeholder="Confirm password" value={password1} 
+                                    required onChange={(e) => setPassword1(e.target.value)}
                                 />
                                 <i className="uil uil-lock password"></i>
                                 <i 
