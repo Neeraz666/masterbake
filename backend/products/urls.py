1ec7ec4543671bf494f900aa31b070205fd3a523
Anish 7777777 <anishpantha9@gmail.com>
2024-05-26 12:37:34 +0545

    changed about jsx and css

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import ListCategory, ListProduct
 
 urlpatterns = [
-    path('<str:category>/', ListProduct.as_view(), name='ListProductByCategory'),
     path('', ListProduct.as_view(), name='ListProduct'),
-    path('categories/', ListCategory.as_view(), name='ListCategory'),
+    path('category/', ListCategory.as_view(), name='ListCategory'),
 ]
