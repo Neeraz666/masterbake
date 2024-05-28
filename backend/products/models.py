223473aa948fe4f6e999c8691c8cd445a1bbfc52
Ajit Shrestha <ajitstha16@gmail.com>
2024-05-28 06:49:33 +0545

    added price field

--- backend/products/models.py
+++ backend/products/models.py
@@ -9,6 +9,7 @@ class Category(models.Model):
     image = models.ImageField(upload_to='categories')
     description = models.TextField(blank=True)  # Changed to TextField
     slug = models.SlugField(max_length = 100, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Add price field
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -90,7 +91,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price at the time of order
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
 
     def __str__(self):
         return f"{self.product.name} in order of {self.order.user.username}"
